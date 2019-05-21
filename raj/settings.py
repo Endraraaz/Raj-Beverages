ed2d220fdec718841c763212371ec2930c122440
Endraraaz <endraraaz@gmail.com>
2019-05-21 15:54:02 +0545

    Update settings.py

--- raj/settings.py
+++ raj/settings.py
@@ -1,5 +1,9 @@
 
 import os
+import django-heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
