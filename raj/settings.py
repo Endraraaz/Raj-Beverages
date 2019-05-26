fcf767d70db4c7d368a2b3c62f3e48dbc089d9b4
endraraaz <endraraaz@gmail.com>
2019-05-26 08:42:02 +0545

    removed heroku settings

--- raj/settings.py
+++ raj/settings.py
@@ -1,9 +1,9 @@
 
 import os
-import django-heroku
+# import django-heroku
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
